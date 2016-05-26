789ca936a3261db043a90009c3d03df191401ee0
Xiang Li <xiangli.cs@gmail.com>
2016-05-26 16:02:37 -0700

    *: better logging

--- group/group.go
+++ group/group.go
@@ -1,6 +1,7 @@
 package group
 
 import (
+	"log"
 	"time"
 
 	"github.com/xiang90/dos/block"
@@ -32,13 +33,15 @@ func (g *Group) syncWithPeers() {
 			if g.Storage.Has(id) {
 				continue
 			}
+			log.Printf("ask missing block %d from peer %s", id, p.URL)
 			err := g.askBlock(p, id)
 			if err != nil {
 				anyerr = err
 			}
 		}
-		if anyerr == nil {
+		if anyerr == nil && rbs.Until > p.progress {
 			p.progress = rbs.Until
+			log.Printf("update progress of peer %s to %d", p.URL, p.progress)
 		}
 	}
 }
@@ -48,6 +51,7 @@ func (g *Group) askBlock(p *Peer, id int) error {
 	if err != nil {
 		return err
 	}
+	log.Printf("got missing block %d from peer %s", id, p.URL)
 	err = g.Storage.Append(&block.Block{ID: id, Blob: b})
 	if err != nil {
 		// internal issue...
